207a09212f57dbc5efcddd2baac8b1e84d608bfe
Helene Konstantine <helenekonstantine@gmail.com>
2018-01-26 22:59:53 +0100

    Make lint happy again
    All I wanna do is JavaScript

--- Assets/Scripts/Player/RotateToMouse.cs
+++ Assets/Scripts/Player/RotateToMouse.cs
@@ -5,50 +5,35 @@ public class RotateToMouse : MonoBehaviour
 {
     public GameObject player;
     public GameObject projectile;
-    public GameObject spawnPoint1;
+    publicGameObject spawnPoint1;
     public float speed;
     private PlayerMovement playerMovement;
     public Vector2 direction;
     public Quaternion rotation;
     public Transform weaponPos;
 
-    
-    
-
-
-    private void Start()
-    {
+    private void Start() {
         playerMovement = player.GetComponent<PlayerMovement>();
-        
-
     }
 
-    void Update()
-    {
+    void Update() {
         Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
-        
-        
         Vector2 exitPoint = new Vector2(spawnPoint1.transform.position.x, spawnPoint1.transform.position.y);
-        
-        
-
         Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y);
         Vector2 direction = target - playerPos;
-        
-        
+
         direction.Normalize();
         Quaternion rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
 
         GunCheck();
-        if (weaponPos != null)
-        {
-            weaponPos.rotation = rotation;
+        if (weaponPos != null) {
+          weaponPos.rotation = rotation;
         }
-        
-        
-        
 
-        
+
+
+
+
 
 
     }

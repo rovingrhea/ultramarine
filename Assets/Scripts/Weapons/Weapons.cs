d26a098e13447f1b6fef9d9c789856d8daf0c67a
Helene Konstantine <helenekonstantine@gmail.com>
2018-01-26 22:08:26 +0100

    Los namos correctors

--- Assets/Scripts/Weapons/Weapons.cs
+++ Assets/Scripts/Weapons/Weapons.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponScript : MonoBehaviour {
+public class Weapons : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }

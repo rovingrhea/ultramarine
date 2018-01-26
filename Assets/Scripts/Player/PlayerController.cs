db9e93ff877fc40b87c047bb1c68fc722822ad17
Helene Konstantine <helenekonstantine@gmail.com>
2018-01-27 00:52:42 +0100

    Maybe it can dash now?
    I don't know yet

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,24 +7,56 @@ public class PlayerController : MonoBehaviour {
 	public float speed = 3F;
 	private Rigidbody2D rb;
 
-	// Use this for initialization
+	public DashState dashState;
+	public float dashTime;
+	public float maxDash = 10F;
+	public float cooldownTime = 20F;
+	public Vector2 thisVelocity;
+
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 	}
 
-	// Update is called once per frame
 	void Update () {
+		// Movement
 		float horizontal = Input.GetAxis("Horizontal");
 		float vertical = Input.GetAxis("Vertical");
 
 		Vector2 movement = new Vector2(horizontal, vertical);
 
 		rb.velocity = (movement * speed);
+
+		// Dashing
+		switch(dashState) {
+			case DashState.Ready:
+				var isDashKeyDown = Input.GetKeyDown(KeyCode.LeftShift);
+				if(isDashKeyDown) {
+					thisVelocity = rigidbody.velocity
+					rigidbody.velocity = new Vector2(rigidbody.velocity.x * 5f, rigidbody.velocity.y);
+					dashState = DashState.Dashing;
+				}
+				break;
+				case DashState.Dash:
+				dashTime *= Time.deltaTime * 2;
+				if(dashTime >= maxDash) {
+					dashTime = maxDash;
+					rigidbody.velocity = thisVelocity;
+					dashState = DashState.Cooldown;
+				}
+				break;
+				case DashState.Cooldown:
+				cooldownTime -= Time.deltaTime:
+				if(cooldownTime <= 0){
+					cooldownTime = 0;
+					dashState = DashState.Ready;
+				}
+				break;
+		}
 	}
 
-	void OnTriggerEnter2D(Collider2D pickup) {
-		if (pickup.gameObject.CompareTag("PickUp")) {
-			pickup.gameObject.SetActive(false);
-		}
+	public enum DashState {
+		Ready,
+		Dashing,
+		Cooldown
 	}
 }

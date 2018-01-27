fbb8c2d645e8a53d6581309f4769e2918cdfd672
Alex <alex@irt.io>
2018-01-27 11:43:50 +0100

    Add camera movement

---
+++ Assets/Scripts/Player/CamereMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamereMovement : MonoBehaviour {
+
+    public GameObject player;
+    public float speed = 2.5f;
+    private Rigidbody2D playerRb;
+
+    private void Start()
+    {
+        playerRb = player.GetComponent<Rigidbody2D>();
+    }
+
+    void Update () {
+        var pos = transform.position;
+        var target = player.transform.position + ((Vector3)playerRb.velocity * 0.5f);
+        pos = Vector3.Lerp(pos, target, speed * Time.deltaTime);
+        pos.z = transform.position.z;
+
+        transform.position = pos;
+	}
+}

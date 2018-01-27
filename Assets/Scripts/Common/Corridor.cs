669608204229f6b670eaf774c3d0cd55a1412225
Helene Konstantine <helenekonstantine@gmail.com>
2018-01-27 21:05:17 +0100

    los namos filos

--- Assets/Scripts/Common/Corridor.cs
+++ Assets/Scripts/Common/Corridor.cs
@@ -10,7 +10,7 @@ public class Corridor
 {
     public int startXPos;         // The x coordinate for the start of the corridor.
     public int startYPos;         // The y coordinate for the start of the corridor.
-    public int corridorLength;            // How many units long the corridor is.
+    public int corridorLength;    // How many units long the corridor is.
     public Direction direction;   // Which direction the corridor is heading from it's room.
 
 

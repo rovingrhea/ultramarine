c404b2ea103b0a223ab265db3358102eaeb2a16c
Helene Konstantine <helenekonstantine@gmail.com>
2018-01-27 18:05:19 +0100

    SÅNN????

--- Assets/Scripts/Common/LevelGenerator.cs
+++ Assets/Scripts/Common/LevelGenerator.cs
@@ -20,23 +20,18 @@ public class LevelGenerator : MonoBehaviour {
 
 	IEnumerator GenerateLevel() {
 		for(int i = 0; i < tileAmount; i++){
-<<<<<<< HEAD
 			float direction = Random.Range(0F, 1F);
 			int tile = Random.Range(tiles.Length, 0);
 
 			callMoveGen(direction);
-			createTile(tile);
-
-=======
-			int direction = Random.Range(0, 3);
 			MoveGen(direction);
->>>>>>> d71bddb3be2436120760841e1e2534d301dff052
+			createTile(tile);
+		}
 			yield return new WaitForSeconds(waitTime);
 		}
 		yield return 0;
 	}
 
-<<<<<<< HEAD
 	void MoveGen(int direction) {
 		switch(direction) {
 		case 0:
@@ -71,25 +66,22 @@ public class LevelGenerator : MonoBehaviour {
 		tileObject = Instantiate(tiles[tileIndex], transform.position, transform.rotation) as GameObject;
 		createdTiles.Add(tileObject.transform.position);
 	}
-=======
-    void MoveGen(int dir)
-    {
-        switch (dir)
-        {
-            case 0:
-                transform.position = new Vector3(transform.position.x, transform.position.y + tileSize, 0);
-                break;
-            case 1:
-                transform.position = new Vector3(transform.position.x + tileSize, transform.position.y, 0);
-                break;
-            case 2:
-                transform.position = new Vector3(transform.position.x, transform.position.y - tileSize, 0);
-                break;
-            case 3:
-                transform.position = new Vector3(transform.position.x - tileSize, transform.position.y, 0);
-                break;
-        }
-    }
->>>>>>> d71bddb3be2436120760841e1e2534d301dff052
+  void MoveGen(int dir)
+  {
+      switch (dir)
+      {
+          case 0:
+              transform.position = new Vector3(transform.position.x, transform.position.y + tileSize, 0);
+              break;
+          case 1:
+              transform.position = new Vector3(transform.position.x + tileSize, transform.position.y, 0);
+              break;
+          case 2:
+              transform.position = new Vector3(transform.position.x, transform.position.y - tileSize, 0);
+              break;
+          case 3:
+              transform.position = new Vector3(transform.position.x - tileSize, transform.position.y, 0);
+              break;
+      }
+  }
 }
-

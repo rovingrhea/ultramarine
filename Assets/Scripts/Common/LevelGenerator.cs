64107df66aa2384ec68307465e67beb0427aa6a9
Helene Konstantine <helenekonstantine@gmail.com>
2018-01-27 16:18:48 +0100

    Such level

--- Assets/Scripts/Common/LevelGenerator.cs
+++ Assets/Scripts/Common/LevelGenerator.cs
@@ -10,6 +10,9 @@ public class LevelGenerator : MonoBehaviour {
 	public int tileAmount;
 	public int tileSize;
 	public float waitTime;
+	public float chanceUp;
+	public float chanceRight;
+	public float chanceDown;
 
 	void Start () {
 		StartCoroutine(GenerateLevel ());
@@ -17,15 +20,19 @@ public class LevelGenerator : MonoBehaviour {
 
 	IEnumerator GenerateLevel() {
 		for(int i = 0; i < tileAmount; i++){
-			int CapsuleDirection2D = Random.Range(0, 3);
-			MoveGen(dir);
+			float direction = Random.Range(0F, 1F);
+			int tile = Random.Range(tiles.Length, 0);
+
+			callMoveGen(direction);
+			createTile(tile);
+
 			yield return new WaitForSeconds(waitTime);
 		}
 		yield return 0;
 	}
 
-	void MoveGen(int dir){
-		switch(dir) {
+	void MoveGen(int direction) {
+		switch(direction) {
 		case 0:
 			transform.position = new Vector3(transform.position.x, transform.position.y + tileSize, 0);
 			break;
@@ -39,5 +46,23 @@ public class LevelGenerator : MonoBehaviour {
 			transform.position = new Vector3(transform.position.x - tileSize, transform.position.y, 0);
 			break;
 		}
+	}
+
+	void callMoveGen(float randomDirection) {
+		if(randomDirection < chanceUp) {
+			MoveGen(0);
+		} else if(randomDirection < chanceRight) {
+			MoveGen(1);
+		} else if(randomDirection < chanceDown) {
+			MoveGen(2);
+		} else {
+			MoveGen(3);
+		}
+	}
+
+	void createTile(int tileIndex) {
+		GameObject tileObject;
+		tileObject = Instantiate(tiles[tileIndex], transform.position, transform.rotation) as GameObject;
+		createdTiles.Add(tileObject.transform.position);
 	}
 }

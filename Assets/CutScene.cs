ed7d7e8a6e26bdf7c158c7d22a348d4ee834da13
Alex <alex@irt.io>
2018-01-28 10:29:59 +0100

    Added temp intro scene

---
+++ Assets/CutScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutScene : MonoBehaviour {
+
+    public float zoom;
+    public float zoomSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, zoom, zoomSpeed * Time.deltaTime / 100);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
